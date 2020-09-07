f3485f77ab3cab1c72348dec5b469cd8ca550240
Tulendeena40 <crockliff@student.unimelb.edu.au>
2020-09-07 14:06:36 +1000

    Updated port and build

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route }
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' ;
 
 import logo from './logo.svg';
 import './App.css';
