23cc6d07aff62d67be96d130ca0286c1bb160f31
Elijah Nicol <elijah_nicol@hotmail.com>
2020-09-21 17:58:40 +1000

    Removed Axios Test

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,27 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import axios from "axios";
-
-axios({
-  method: 'get',
-  url: 'http://localhost:8000/profile/login',
-  params: {
-    emailAddress: "test@gmail.com",
-    password: "yikes"
-  }
-  })
-  .then(function (response) {
-      //handle success
-      console.log(response.data.result);
-      if(response.data.result == "Success") {
-        console.log("LOGGED ON");
-      }
-  })
-  .catch(function (response) {
-      //handle error
-      console.log(response);
-  });
 
 ///
 ReactDOM.render(
