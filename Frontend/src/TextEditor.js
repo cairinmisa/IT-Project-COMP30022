cf3d24403fbc0478e98b700be5c5b99b568a3575
Raphi-Wirth <rwirth@student.unimelb.edu.au>
2020-09-24 13:38:24 +1000

    Template functionality added
    Can now click on icons to change the current template

--- Frontend/src/TextEditor.js
+++ Frontend/src/TextEditor.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 import CKEditor from "@ckeditor/ckeditor5-react";
 import InlineEditor from "@ckeditor/ckeditor5-build-inline";
-
-
-
+import template2 from "./templates/template2.js";
+import template1 from "./templates/template.js";
 
 export default class TextEditor extends Component {
+  state = {
+    currentTemplate : template1
+  }
+  constructor(props){
+    super(props);
+    this.state = {currentTemplate : template1}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(templateClicked) {
+    console.log(templateClicked);
+    this.setState({currentTemplate : templateClicked})
+  }
+
   render() {
+
     return (
       <div className="text-editor">
-        
+        <ul>
+            <li onClick = {() => this.handleClick(template1)}>Template 1</li>
+            <li onClick = {() => this.handleClick(template2)}>Template 2</li>
+        </ul>
         <h2>Create a portfolio below.</h2>
         <div className="editorComponent">
-        
           <CKEditor
             editor={InlineEditor}
             data="<h1>John Smith</h1>"
@@ -23,12 +39,9 @@ export default class TextEditor extends Component {
           />
         </div>
         <div className="editorComponent">
-        <link rel="stylesheet" href="./Ck.css" type="text/css"></link>
           <CKEditor
             editor={InlineEditor}
-            data="<div>
-                    ello there
-                  </div>"
+            data= {this.state.currentTemplate}
             onInit={(editor) => {
               // You can store the "editor" and use when it is needed.
               console.log("Editor is ready to use!", editor);
@@ -40,4 +53,3 @@ export default class TextEditor extends Component {
     );
   }
 }
-
