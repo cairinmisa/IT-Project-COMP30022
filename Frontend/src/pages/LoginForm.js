0aa76325a7f2b7dc9fdd9d20db057ac220632e92
Elijah Nicol <elijah_nicol@hotmail.com>
2020-09-27 14:45:00 +1000

    Axios Request and Login
    Allowed user to login using an axios request.

--- Frontend/src/pages/LoginForm.js
+++ Frontend/src/pages/LoginForm.js
@@ -3,6 +3,7 @@ import InputField from "./InputField";
 import SubmitButton from "./SubmitButton";
 import UserStore from "../stores/UserStore";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class LoginForm extends Component {
   constructor(props) {
@@ -39,41 +40,78 @@ class LoginForm extends Component {
     });
   }
 
+  handleResponse(response) {
+    //handle success
+    console.log(this);
+    console.log(response.data);
+    if(response.data.result == "Success") {
+      UserStore.isLoggedIn = true;
+      UserStore.username = response.data.user.username;
+    }
+    else {
+      alert("Incorrect Password");
+      this.resetForm();
+    }
+  }
+
+  handleResponseError(response) {
+    //handle error
+    console.log(response);
+    this.resetForm();
+  }
+
   async doLogin() {
+    // If user hasn't specified an email then return
     if (!this.state.email) {
       return;
     }
+
+    // If user hasn't specified a password then return
     if (!this.state.password) {
       return;
     }
-    this.setState({
-      buttonDisbaled: true
-    });
-    try {
-      let res = await fetch("/login", {
-        method: "post",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application"
-        },
-        body: JSON.stringify({
-          email: this.state.email,
-          password: this.state.password
-        })
-      });
 
-      let result = await res.json();
-      if (res && result.success) {
-        UserStore.isLoggedIn = true;
-        UserStore.username = result.username;
-      } else if (res && result.success === false) {
-        this.resetForm();
-        alert(result.msg);
+    this.setState({
+      buttonDisabled: true
+    });
+
+    // Fetch user from database
+    axios({
+      method: 'get',
+      url: 'http://localhost:8000/profile/login',
+      params: {
+        emailAddress: this.state.email,
+        password: this.state.password
       }
-    } catch (e) {
-      console.log(e);
-      this.resetForm();
-    }
+      })
+      .then(response => this.handleResponse(response))
+      .catch(response => this.handleResponseError(response));
+
+    // try {
+    //   let res = await fetch("/login", {
+    //     method: "post",
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application"
+    //     },
+    //     body: JSON.stringify({
+    //       email: this.state.email,
+    //       password: this.state.password
+    //     })
+    //   });
+
+    //   let result = await res.json();
+    //   if (res && result.success) {
+    //     UserStore.isLoggedIn = true;
+    //     UserStore.username = result.username;
+    //   } else if (res && result.success === false) {
+    //     this.resetForm();
+    //     alert(result.msg);
+    //   }
+    // } catch (e) {
+    //   console.log(e);
+    //   this.resetForm();
+    // }
   }
 
   state = {};
