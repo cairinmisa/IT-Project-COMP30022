0f4ef1d3949bd35d6b20cc4184dcf593d17d8584
Elijah Nicol <elijah_nicol@hotmail.com>
2020-09-28 14:52:41 +1000

    Added Profile Information
    Added profile information to profile page for debugging purposes.

--- Frontend/src/pages/ProfilePage.js
+++ Frontend/src/pages/ProfilePage.js
@@ -17,6 +17,11 @@ class ProfilePage extends Component {
         return (
             <div>
                 <h1>Welcome {this.capitaliseName(UserStore.user.firstName)}</h1>
+                <h2>Full Name: {this.capitaliseName(UserStore.user.firstName)} {this.capitaliseName(UserStore.user.lastName)}</h2>
+                <h3>Username: {UserStore.user.username}</h3>
+                <h3>Email: {UserStore.user.emailAddress}</h3>
+                <h3>Date of Birth: {UserStore.user.dOB ? UserStore.user.dOB : "Not Specified"}</h3>
+                {console.log(UserStore.user)}
                 <button onClick={() => this.doLogout()}>Logout</button>
             </div>
         );
