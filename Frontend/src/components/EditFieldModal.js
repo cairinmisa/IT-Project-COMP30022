8ff092c776fa5304effb7bd2e5bf4b3c063b82cc
Elijah Nicol <elijah_nicol@hotmail.com>
2020-10-02 22:51:59 +1000

    Setup Request to Server
    Got structure going for sending requests to the server, now just waiting to correct the request such that it is correct.

--- Frontend/src/components/EditFieldModal.js
+++ Frontend/src/components/EditFieldModal.js
@@ -1,16 +1,77 @@
 import React from 'react';
 import InputField from "../pages/InputField";
+import axios from "axios";
+import {host} from "../stores/Settings"
+import UserStore from "../stores/UserStore";
 
 class EditFieldModal extends React.Component {
     state = {
         input: null,
-        password: null
+        password: null,
+        password2: null
     };
     resetModal = false;
 
-    // Send edit request to server
-    submitValue(whichField, input, password) {
+    resetForm() {
+        this.setState({
+            input: this.props.prevInput,
+            password: null,
+            password2: null
+        });
+    }
 
+    handleResponse(response) {
+        console.log(response);
+        // Modifying success
+        if(!response.data.hasErrors) {
+            // Submit if everything went smoooth
+            this.props.onSubmit();
+
+            // Also force app reload so data is up to date
+            window.location.reload(false);
+        } else if(response.data.hasErrors) {
+            this.resetForm();
+        }
+    }
+
+    handleResponseError(response) {
+        console.log(response);
+        alert("An unknown error has occured.");
+        this.resetForm();
+    }
+
+    // Send edit request to server
+    async submitValue(whichField, input, password, password2) {
+        // Check if all fields have input
+        if(input != "" && password != "" && password2 != ""){
+            // Make a value being changed property
+            let valueModifying = "";
+            if(whichField === "First Name") {
+                valueModifying = "firstName";
+            } else if(whichField === "Last Name") {
+                valueModifying = "lastName";
+            } else if(whichField === "Email") {
+                valueModifying = "emailAddress";
+            } else if(whichField === "Username") {
+                valueModifying = "username";
+            } else if(whichField === "Date of Birth") {
+                valueModifying = "dob";
+            }
+            
+            // Run check to server
+            await axios({
+                method: 'put',
+                url:  host+'/profile/update',
+                data: {
+                    userID: UserStore.user.userID,
+                    oldpassword: password,
+                    oldpassword2: password2,
+                    [valueModifying]: input
+                }
+                })
+                .then(response => this.handleResponse(response))
+                .catch(response => this.handleResponseError(response));
+        }
     }
 
     render() {
@@ -21,6 +82,7 @@ class EditFieldModal extends React.Component {
         else if (!this.resetModal) {
             this.state.input = this.props.prevInput;
             this.state.password = null;
+            this.state.password2 = null;
             this.resetModal = true;
         }
         return (
@@ -42,7 +104,13 @@ class EditFieldModal extends React.Component {
                                 value={this.state.password ? this.state.password : ""}
                                 onChange={(val) => this.setState({password:val})}
                             ></InputField>
-                            <button onClick={()=> this.props.onSubmit()}>Submit</button>
+                            <InputField
+                                type="password"
+                                placeholder="Confirm Password"
+                                value={this.state.password2 ? this.state.password2 : ""}
+                                onChange={(val) => this.setState({password2:val})}
+                            ></InputField>
+                            <button onClick={()=> this.submitValue(this.props.whichField, this.state.input, this.state.password, this.state.password2)}>Submit</button>
                         </div>
                     </div>
                 </div>
