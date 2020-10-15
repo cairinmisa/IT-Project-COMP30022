5caf7f010157f1fe4df068bf4ffe80a291d8cb5f
Tulendeena40 <crockliff@student.unimelb.edu.au>
2020-10-15 12:55:17 +1100

    Changed Template to no longer attach to an eportfolio

--- Backend/controllers/template.js
+++ Backend/controllers/template.js
@@ -13,25 +13,25 @@ exports.register = async (req,res,next) =>{
     //Password exists
     let response = {}
     let result = {}
-    /* TITLE CHECK
-    await Eportfolio.find({userID : req.body.userID, title : req.body.title}).then(async function(list){
+    //TITLE CHECK
+    await Template.find({userID : req.body.userID, title : req.body.title}).then(async function(list){
         result = list
     });
 
     if(result.length != 0){
         return res.send({titleExists : "True", hasErrors : "True"})
     }
-    */
 
     // Eportfolio Check, Checks if eportfolio already exists in db (with that userID)
-    await Eportfolio.find({eportID : req.body.eportID, userID : req.body.userID}).then(async function(list){
-        result = list
+    await Eportfolio.findOne({eportID : req.body.eportID, userID : req.body.userID}).then(async function(list){
+        if(list != null){
+            req.body.data = list.data;
+        }
     });
-    if(result.length == 0){
+    if(result == null){
         return res.send({hasErrors : "False", eportExists : "False"})
     }
 
-    
     // Set Default values for other fields
     if(req.body.isPublic == null){
         req.body.isPublic = "False"
@@ -43,6 +43,8 @@ exports.register = async (req,res,next) =>{
     req.body.ratingTotal = 0
     req.body.rating = 0
     req.body.ratedUsers = []
+
+
     // Generate new templateID
     testID = await Template.countDocuments() + 1;
     while(1){
@@ -52,18 +54,14 @@ exports.register = async (req,res,next) =>{
           }
           testID ++;
     }
-    // Create new eportfolio by copying the data from the old one
-    neweportID = await eportController.copy(req.body.eportID);
-    await Eportfolio.updateOne({eportID : neweportID}, {templateID : req.body.templateID});
-    req.body.eportID = neweportID;
-    
+
     // Create the new template, and respond with its details
     Template.create(req.body).then(function(template){
-
         response.hasErrors = "False";
         response.eportID = template.eportID;
         response.isPublic = template.isPublic;
         response.comments = template.comments;
+        response.data = template.data;
         response.title = template.title;
         response.category = template.category;
         response.userID = template.userID;
