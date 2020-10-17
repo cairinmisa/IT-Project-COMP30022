e51bf4bb0abf196d32e6e6e721dcb32755a06b8c
Elijah Nicol <elijah_nicol@hotmail.com>
2020-10-17 15:06:40 +1100

    Throw Not Logged In Error
    If a user hasn't logged in, prompt them to log in or sign up when selecting templates on the templates page.

--- Frontend/src/components/TemplateToFolioModal.js
+++ Frontend/src/components/TemplateToFolioModal.js
@@ -1,6 +1,8 @@
 // This modal will pop up when a user wants to create a folio from a template
 import React, { Component } from 'react';
 import InputField from "../pages/InputField";
+import UserStore from "../stores/UserStore";
+import { Link } from "react-router-dom";
 
 class TemplateToFolioModal extends Component {
     state = {
@@ -12,7 +14,21 @@ class TemplateToFolioModal extends Component {
         this.props.createPortfolio(this.state.input, this.state.publicity)
     }
 
-    render() { 
+    render() {
+        if(UserStore.isLoggedIn == false) {
+            return (  
+                <div className="greyOutBG">
+                     <div className="modal">
+                     <button className="closeBtn" onClick={()=> this.props.closeCreateNew()}>X</button>
+                        <div className="modal-content">
+                            <h2><span className="blue">Join</span> our platform</h2>
+                            <Link to="login" className="btn whiteBG">Log in</Link>
+                            <Link to="signup" className="btn redBG">Sign up</Link>
+                        </div>
+                    </div>
+                </div>
+            );
+        } 
         return (  
             <div className="greyOutBG">
                  <div className="modal">
