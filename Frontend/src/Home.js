7fa13afa1f63dc4f021c3296d4b3e5985b06b112
Elijah Nicol <elijah_nicol@hotmail.com>
2020-10-25 23:41:40 +1100

    Updated Landing Page
    Landing page now has a few updates:
    - updated images to reflect actual workspace
    - added emojis for style points

--- Frontend/src/Home.js
+++ Frontend/src/Home.js
@@ -19,26 +19,26 @@ class Home extends Component {
         <div className="highlight">
           <div className="highlight-text">
             <h2>
-              <img src={require("./images/highlight-2.png")} alt="Portfolio Highlight 1" />A{" "}
-              <span className="blue">new way</span> to create portfolios.
+              <img src={require("./images/highlight.PNG")} alt="Portfolio Highlight 1" />
+              👏 A{" "}<span className="blue">new way</span> to create portfolios 👏
             </h2>
             <h3>
               Our easy to use workspace makes creating displays of your work a
               breeze, so you can focus on job applications instead of spending
-              hours aligning an image.
+              hours aligning an image in Word.
             </h3>
           </div>
         </div>
         <div className="highlight">
           <div className="highlight-text">
             <h2>
-              <img src={require("./images/highlight-2.png")} alt="Portfolio Highlight 2" />
-              <span className="blue">Show off</span> your work.
+              <img src={require("./images/highlight-4.PNG")} alt="Portfolio Highlight 2" />
+              <span className="blue">🚀 Show off</span> your work in minutes 🚀
             </h2>
             <h3>
               We provide plenty of ready to go templates so that you don't need
               to worry about creating. Select a template, update it with your
-              projects, and you will be good to go.
+              projects/details, and you will be good to go.
             </h3>
           </div>
         </div>
