334c4f550fe7ec07bf82a265ab8ec93233210f05
Elijah Nicol <elijah_nicol@hotmail.com>
2020-10-29 14:06:34 +1100

    Personal Page Overhaul
    Personal profile page now has the same layout as the general public user page. However, added a modify button

--- Frontend/src/pages/UserPage.js
+++ Frontend/src/pages/UserPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import UserStore from "../stores/UserStore";
 import Axios from "axios";
 import {host} from "../stores/Settings";
+import SubmitButton from "./SubmitButton";
+import { Redirect } from 'react-router-dom';
 
 class UserPage extends Component {
     state = { 
@@ -10,7 +12,9 @@ class UserPage extends Component {
         fullName : "",
         userID : "",
         portfolios: [],
-        templates: []
+        templates: [],
+        isLoggedInUser : false,
+        redirectToModify : false
      }
 
      async getPortfolios(ID){
@@ -62,27 +66,50 @@ class UserPage extends Component {
         this.getUser(this.props.email)
         console.log(this.props.userID)
         this.getPortfolios(this.props.userID)
+        if(this.state.email === UserStore.user.emailAddress) {
+          this.setState({
+            isLoggedInUser : true
+          });
+        }
     }
 
     render() { 
+        if(this.state.redirectToModify) {
+          return (
+            <Redirect to="/accountinfo" />
+          )
+        }
         return ( 
             <div className = "accountForm">
               <div className ="accountForm-content">
+                {/* Name of User */}
                 <h1>✌🏼 {this.state.fullName} ✌🏼</h1>
                 <p className="username">@{this.state.userName}</p>
                 
+                {/* Public Portfolio List */}
                 <p className="bold">Public Folios:</p>
                 {
                   this.state.portfolios.length !== 0
                     ? this.state.portfolios.map((portfolio, i) => <li className="folioTemplate-li" key={i}>{portfolio[1]}</li>)
-                    : "User does not have any public folios."
+                    : (this.state.isLoggedInUser ? "You do not have any public folios." : "User does not have any public folios.")
                 }
 
+                {/* Public Template List */}
                 <p className="bold">Public Templates:</p>
                 {
                   this.state.templates.length !== 0
                     ? this.state.templates.map((template, i) => <li className="folioTemplate-li" key={i}>{template[1]}</li>)
-                    : "User does not have any public templates."
+                    : (this.state.isLoggedInUser ? <p>You do not have any public templates.</p> : <p>User does not have any public templates.</p>)
+                }
+
+                {/* Button to modify user if logged in */}
+                {
+                  this.state.isLoggedInUser 
+                  ? <SubmitButton
+                    text="Modify Account" derivedClass="whiteBG"
+                    onClick={() => this.setState({redirectToModify : true})}
+                    ></SubmitButton>
+                  : null
                 }
               </div>
             </div>
