820145e4c0c116f8bb9940b9aae9a48f2cca7215
Elijah Nicol <ejnicol@student.unimelb.edu.au>
2020-11-02 12:51:55 +1100

    Capitalised PDF

--- Frontend/src/components/WorkspaceToolbar.js
+++ Frontend/src/components/WorkspaceToolbar.js
@@ -44,7 +44,7 @@ class WorkspaceToolbar extends Component {
             <ReactToPdf scale={0.78} filename={this.props.folioTitle + ".pdf"}>
               {({ toPdf, targetRef }) => (
                 <span>
-                  <button onClick={() => this.generatePdf(toPdf)}>Generate pdf</button>
+                  <button onClick={() => this.generatePdf(toPdf)}>Generate PDF</button>
                   <div ref={targetRef}>
                     {this.state.displayPdf ? <CKEditor
                       editor={BalloonBlockEditor}
@@ -74,7 +74,7 @@ class WorkspaceToolbar extends Component {
           <ReactToPdf scale={0.78} filename={this.props.folioTitle + ".pdf"}>
             {({ toPdf, targetRef }) => (
               <span>
-                <button onClick={() => this.generatePdf(toPdf)}>Generate pdf</button>
+                <button onClick={() => this.generatePdf(toPdf)}>Generate PDF</button>
                 <div ref={targetRef}>
                   {this.state.displayPdf ? <CKEditor
                     editor={BalloonBlockEditor}
